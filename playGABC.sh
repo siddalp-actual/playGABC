e3c4fdf7688d39c6897a20b664620642d05b26b0
siddalp-actual <76445019+siddalp-actual@users.noreply.github.com>
2025-04-14 13:42:50 +0100

    pass flags through invoking script

--- playGABC.sh
+++ playGABC.sh
@@ -1,10 +1,29 @@
 #! /bin/bash
 echo "args: $@"
 thisdir=`pwd`
+file_path=""
+args=()
+# Parse the arguments, looking for the file path.
+while [ "$#" -gt 0 ]; do
+  case "$1" in
+    -* | --*) # If it starts with - or --, treat it as an option.
+      args+=("$1")
+      ;;
+    *)       # Otherwise, assume it's the file path.
+      if [ -z "$file_path" ]; then
+        file_path="$1"
+      else
+        args+=("$1") # If file_path is already found, treat this as an argument.
+      fi
+      ;;
+  esac
+  shift
+done
+
 mydir=`dirname $0`
 fn="playGABC"
 pushd "/home/siddalp/github/playGABC"
-./parse_gabc.py "$thisdir/$1" > $fn.ly
+./parse_gabc.py "${args[@]}" "$thisdir/$file_path" > $fn.ly
 lilypond $fn.ly
 timidity $fn.midi
 popd
